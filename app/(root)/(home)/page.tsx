6f0a21f87ed7cfb33fc4279c8da12cd72b05e48f
Sreedharan Namboodiri C <sreedharan@braincade.in>
2023-12-08 16:53:19 +0530

    Created Navbar and Mobile Navbar

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { UserButton } from "@clerk/nextjs";
 
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
-      <UserButton afterSignOutUrl="/" />
     </div>
   );
 };
